9e58d48d9d39bc745d9b14c9ae527e7150c8beeb
exaby73 <nabeelparkar99@gmail.com>
2023-10-15 14:04:16 +0530

    feat: flutter keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 require("config.osKeyMap").setup()
 local map = require("config.osKeyMap").mapping
+local lazyUtil = require("lazyvim.util")
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
@@ -13,3 +14,20 @@ keymap("n", map.OPT_SHIFT_BACKSLASH, "<cmd>FlutterRestart<cr>", opts)
 
 -- Keep cursor in the same place for yank
 keymap("v", "y", "ygv<Esc>", opts)
+
+-- Flutter
+keymap("n", "<leader>fs", ":FlutterRun<CR>", opts)
+keymap("n", "<leader>fo", ":FlutterOutlineToggle<CR>", opts)
+keymap("n", "<leader>fd", ":FlutterDevices<CR>", opts)
+keymap("n", "<leader>fe", ":FlutterEmulators<CR>", opts)
+keymap("n", "<leader>ft", ":FlutterOpenDevTools<CR>", opts)
+keymap("n", "<leader>fr", ":FlutterReload<CR>", opts)
+keymap("n", "<leader>fR", ":FlutterRestart<CR>", opts)
+
+local lazyterm = function()
+  lazyUtil.terminal(nil, { cwd = lazyUtil.root() })
+end
+keymap("n", "<c-_>", function()
+  lazyUtil.terminal()
+end, { desc = "Toggle terminal (cwd)" })
+keymap("n", "<leader>fT", lazyterm, { desc = "Terminal (root dir)" })
