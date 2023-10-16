a02ce3a7ef8a869ccc2e95923c08e75500b238b4
exaby73 <nabeelparkar99@gmail.com>
2023-10-16 20:37:50 +0530

    feat: move flutter keymaps to lazy keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,32 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-require("config.osKeyMap").setup()
-local map = require("config.osKeyMap").mapping
 local lazyUtil = require("lazyvim.util")
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
-keymap("n", map.OPT_BACKSLASH, "<cmd>FlutterReload<cr>", opts)
-keymap("n", map.OPT_SHIFT_BACKSLASH, "<cmd>FlutterRestart<cr>", opts)
-
 -- Keep cursor in the same place for yank
 keymap("v", "y", "ygv<Esc>", opts)
 
--- Flutter
-keymap("n", "<leader>fs", ":FlutterRun<CR>", opts)
-keymap("n", "<leader>fo", ":FlutterOutlineToggle<CR>", opts)
-keymap("n", "<leader>fd", ":FlutterDevices<CR>", opts)
-keymap("n", "<leader>fe", ":FlutterEmulators<CR>", opts)
-keymap("n", "<leader>ft", ":FlutterOpenDevTools<CR>", opts)
-keymap("n", "<leader>fr", ":FlutterReload<CR>", opts)
-keymap("n", "<leader>fR", ":FlutterRestart<CR>", opts)
-
+-- Floating terminal remaps
 local lazyterm = function()
   lazyUtil.terminal(nil, { cwd = lazyUtil.root() })
 end
+
 keymap("n", "<c-_>", function()
   lazyUtil.terminal()
 end, { desc = "Toggle terminal (cwd)" })
+
 keymap("n", "<leader>fT", lazyterm, { desc = "Terminal (root dir)" })
--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -1,6 +1,47 @@
+require("config.osKeyMap").setup()
+local map = require("config.osKeyMap").mapping
+
 return {
   "akinsho/flutter-tools.nvim",
   lazy = false,
+  keys = {
+    {
+      "<leader>fs",
+      "<cmd>FlutterRun<cr>",
+      desc = "Start Flutter",
+    },
+    {
+      "<leader>fd",
+      "<cmd>FlutterOpenDevTools<CR>",
+      desc = "Open Flutter Devtools",
+    },
+    {
+      "<leader>fr",
+      "<cmd>FlutterReload<CR>",
+      desc = "Hot Reload Flutter",
+    },
+    {
+      map.OPT_BACKSLASH,
+      "<cmd>FlutterReload<cr>",
+      desc = "Hot Reload Flutter",
+    },
+    {
+      "<leader>fR",
+      "<cmd>FlutterRestart<CR>",
+      desc = "Hot Restart Flutter",
+    },
+    {
+      map.OPT_SHIFT_BACKSLASH,
+      "<cmd>FlutterRestart<cr>",
+      desc = "Hot Restart Flutter",
+    },
+
+    {
+      "<leader>fq",
+      "<cmd>FlutterQuit<CR>",
+      desc = "Quit Flutter",
+    },
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim", -- optional for vim.ui.select
