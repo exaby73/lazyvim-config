b1d3b5c576a81d0a6d44c0f5ab05c9e0f961f74d
exaby73 <nabeelparkar99@gmail.com>
2023-10-16 20:38:50 +0530

    feat: keymaps for switching buffers

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,43 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    {
+      "<leader>1",
+      "<cmd>BufferLineGoToBuffer 1<cr>",
+      desc = "Go to buffer 1",
+    },
+    {
+      "<leader>2",
+      "<cmd>BufferLineGoToBuffer 2<cr>",
+      desc = "Go to buffer 2",
+    },
+    {
+      "<leader>3",
+      "<cmd>BufferLineGoToBuffer 3<cr>",
+    },
+    {
+      "<leader>4",
+      "<cmd>BufferLineGoToBuffer 4<cr>",
+    },
+    {
+      "<leader>5",
+      "<cmd>BufferLineGoToBuffer 5<cr>",
+    },
+    {
+      "<leader>6",
+      "<cmd>BufferLineGoToBuffer 6<cr>",
+    },
+    {
+      "<leader>7",
+      "<cmd>BufferLineGoToBuffer 7<cr>",
+    },
+    {
+      "<leader>8",
+      "<cmd>BufferLineGoToBuffer 8<cr>",
+    },
+    {
+      "<leader>9",
+      "<cmd>BufferLineGoToBuffer 9<cr>",
+    },
+  },
+}
