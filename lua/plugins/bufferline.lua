7b9a92e328b7e637ddd92bfed8dd8dacc1b83098
exaby73 <nabeelparkar99@gmail.com>
2023-10-17 17:14:27 +0530

    feat: bufferline keymaps

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,6 @@
 return {
   "akinsho/bufferline.nvim",
+
   keys = {
     {
       "<leader>1",
@@ -14,30 +15,37 @@ return {
     {
       "<leader>3",
       "<cmd>BufferLineGoToBuffer 3<cr>",
+      desc = "Go to buffer 3",
     },
     {
       "<leader>4",
       "<cmd>BufferLineGoToBuffer 4<cr>",
+      desc = "Go to buffer 4",
     },
     {
       "<leader>5",
       "<cmd>BufferLineGoToBuffer 5<cr>",
+      desc = "Go to buffer 5",
     },
     {
       "<leader>6",
       "<cmd>BufferLineGoToBuffer 6<cr>",
+      desc = "Go to buffer 6",
     },
     {
       "<leader>7",
       "<cmd>BufferLineGoToBuffer 7<cr>",
+      desc = "Go to buffer 7",
     },
     {
       "<leader>8",
       "<cmd>BufferLineGoToBuffer 8<cr>",
+      desc = "Go to buffer 8",
     },
     {
       "<leader>9",
       "<cmd>BufferLineGoToBuffer 9<cr>",
+      desc = "Go to buffer 9",
     },
   },
 }
