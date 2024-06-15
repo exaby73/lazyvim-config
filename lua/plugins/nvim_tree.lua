eea9e22d43f325dc60b4f38f6d732cdce2062ea6
exaby73 <nabeelparkar99@gmail.com>
2024-06-15 19:55:46 +0530

    fix: nvim-tree custom keymaps

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,3 +1,5 @@
+local tables = require "utils.tables"
+
 return {
   "nvim-tree/nvim-tree.lua",
   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
@@ -6,26 +8,32 @@ return {
   end,
   config = function(_, opts)
     dofile(vim.g.base46_cache .. "nvimtree")
-    require("nvim-tree").setup {
-      on_attach = function()
-        local nvimtreeapi = require "nvim-tree.api"
+    opts["on_attach"] = function(bufnr)
+      local nvimtreeapi = require "nvim-tree.api"
 
-        local function edit_or_open()
-          local node = nvimtreeapi.tree.get_node_under_cursor()
+      nvimtreeapi.config.mappings.default_on_attach(bufnr)
 
-          if node.nodes ~= nil then
-            -- expand or collapse folder
-            nvimtreeapi.node.open.edit()
-          else
-            -- open file
-            nvimtreeapi.node.open.edit()
-            -- Close the tree if file was opened
-            nvimtreeapi.tree.close()
-          end
+      local function edit_or_open()
+        local node = nvimtreeapi.tree.get_node_under_cursor()
+
+        if node.nodes ~= nil then
+          -- expand or collapse folder
+          nvimtreeapi.node.open.edit()
+        else
+          -- open file
+          nvimtreeapi.node.open.edit()
+          -- Close the tree if file was opened
+          nvimtreeapi.tree.close()
         end
+      end
 
-        vim.keymap.set("n", "e", edit_or_open, { desc = "Edit Or Open" })
-      end,
-    }
+      local function make_opts(desc)
+        return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      vim.keymap.set("n", "e", edit_or_open, make_opts "Edit file or open folder")
+    end
+
+    require("nvim-tree").setup(opts)
   end,
 }
